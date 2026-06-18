bfef51f9dd1a559521949cc799f60770cf076e6b
lukakhaja <lukakhaja@yahoo.com>
2026-06-18 22:39:40 +0400

    bitasmbl add: Implement navigation routing

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -4,7 +4,9 @@ export default function router({ items }: { items: string[] }) {
       <ul className="p-2 flex flex-col list-disc space-y-2 text-lg">
         {items.map((item) => {
           return (
-            <li key={item}>{item} yoo bruh, point, on, feracon negga, stfu</li>
+            <li key={item}>
+              {item} yoo bruh, point, on, feracon negga, stfu, ma negga
+            </li>
           );
         })}
       </ul>
