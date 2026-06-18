3af5cd973f215073f1f62b104eea715d8fd5100a
lukakhaja <lukakhaja@yahoo.com>
2026-06-18 15:37:20 +0400

    bitasmbl add: Implement navigation routing

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-
 export default function router({ items }: { items: string[] }) {
   return (
     <div>
       <ul className="p-2 flex flex-col list-disc space-y-2 text-lg">
         {items.map((item) => {
-          return <li key={item}>{item} yoo bruh, point, on, feracon negga</li>;
+          return (
+            <li key={item}>{item} yoo bruh, point, on, feracon negga, stfu</li>
+          );
         })}
       </ul>
     </div>
