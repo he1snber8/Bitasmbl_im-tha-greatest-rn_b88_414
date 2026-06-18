ecc030dc3fb18f4e3beed1b33305f4615dcc1871
lukakhaja <lukakhaja@yahoo.com>
2026-06-18 23:37:16 +0400

    bitasmbl add: Implement navigation routing

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -6,7 +6,19 @@ export default function router({ items }: { items: string[] }) {
           return (
             <li key={item}>
               {item} yoo bruh, point, on, feracon negga, stfu, ma negga, for
-              tha, pull up to tha side city, fuck you!!!, hello hello !!!, lol
+              tha, pull up to tha side city, fuck you!!!, hello hello !!!, lol,
+              yeah
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="p-2 flex flex-col list-disc space-y-2 text-lg">
+        {items.map((item) => {
+          return (
+            <li key={item}>
+              {item} yoo bruh, point, on, feracon negga, stfu, ma negga, for
+              tha, pull up to tha side city, fuck you!!!, hello hello !!!, lol,
+              yeah
             </li>
           );
         })}
