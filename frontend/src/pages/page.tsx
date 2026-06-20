0862e9e4e67611ae8e54954f68a7511752f434c8
lukakhaja <lukakhaja@yahoo.com>
2026-06-20 11:49:24 +0400

    bitasmbl add: Define portfolio layout

--- frontend/src/pages/page.tsx
+++ frontend/src/pages/page.tsx
@@ -38,6 +38,18 @@ export default function HomePage() {
           <p>Some project information here 2</p>
         </section>
       </main>
+      <main>
+        <section>
+          <h1>Hero Section2</h1>
+          <p>Welcome to my portfolio. 2</p>
+          <p>Welcome to my portfolio. 2</p>
+        </section>
+
+        <section>
+          <h2>Content Section</h2>
+          <p>Some project information here 2</p>
+        </section>
+      </main>
 
       <footer>
         <p>© 2026 Portfolioo</p>
