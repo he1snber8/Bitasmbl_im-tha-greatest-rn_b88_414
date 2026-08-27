4cedd5c04fada795cbd6194641bd422964ec6174
lukakhaja <lukakhaja@yahoo.com>
2026-08-27 17:31:06 +0400

    add: files

--- frontend/src/test-diff.ts
+++ frontend/src/test-diff.ts
@@ -1,13 +1,13 @@
-import fs from "fs";
-import parseGitDiff from "parse-git-diff";
+// import fs from "fs";
+// import parseGitDiff from "parse-git-diff";
 
-const diff = fs.readFileSync("patch.diff", "utf8");
+// const diff = fs.readFileSync("patch.diff", "utf8");
 
-console.log(diff.slice(0, 300));
+// console.log(diff.slice(0, 300));
 
-const parsed = parseGitDiff(diff);
+// const parsed = parseGitDiff(diff);
 
-console.dir(parsed, {
-  depth: null,
-  colors: true,
-});
+// console.dir(parsed, {
+//   depth: null,
+//   colors: true,
+// });
--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -1,26 +1,26 @@
-import { Project } from "ts-morph";
-import path from "path";
-import { fileURLToPath } from "url";
+// import { Project } from "ts-morph";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const project = new Project();
+// const project = new Project();
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const source = project.addSourceFileAtPath(
-  path.join(__dirname, "router/router.tsx"),
-);
+// const source = project.addSourceFileAtPath(
+//   path.join(__dirname, "router/router.tsx"),
+// );
 
-console.log("Imports:");
-source.getImportDeclarations().forEach((i) => {
-  console.log("-", i.getModuleSpecifierValue());
-});
+// console.log("Imports:");
+// source.getImportDeclarations().forEach((i) => {
+//   console.log("-", i.getModuleSpecifierValue());
+// });
 
-console.log("\nFunctions:");
-source.getFunctions().forEach((f) => {
-  console.log("-", f.getName());
-});
+// console.log("\nFunctions:");
+// source.getFunctions().forEach((f) => {
+//   console.log("-", f.getName());
+// });
 
-console.log("\nVariables:");
-source.getVariableDeclarations().forEach((v) => {
-  console.log("-", v.getName());
-});
+// console.log("\nVariables:");
+// source.getVariableDeclarations().forEach((v) => {
+//   console.log("-", v.getName());
+// });
